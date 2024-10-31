fa8c7a0a3df23bc6e4d1c771e17c3a86486e0360
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2024-10-30 20:11:11 -0400

    bascially done

--- hw2-echo-shell/server.c
+++ hw2-echo-shell/server.c
@@ -14,30 +14,24 @@ static uint8_t send_buffer[BUFF_SZ];
 static uint8_t recv_buffer[BUFF_SZ];
 
 /*
- *  A very simple database structure for this assignment, yes, i 
- *  know its hard coded, but its not the main part of this
- *  assignment so it was easist.  The format is the first
- *  field is a key, the second is a value.  In java we 
- *  would be simulating a hashmap. See the server.h file
- *  for the definition of course_item_t 
+ *  A simple database structure for this assignment with hardcoded courses.
+ *  The format is: first field is a key, the second is a value.
  */
 static course_item_t course_db[] = {
     {"cs472", "CS472: Welcome to computer networks"},
     {"cs281", "CS281: Hello from computer architecture"},
     {"cs575", "CS575: Software Design is fun"},
-    {"cs577", "CS577: Software architecture is important"}
+    {"cs577", "cs577: Software architecture is important"}
 };
 
 /*
- *  Helper, given a course_id, returns the item from the course_db[]
- *  array that matches, if no match, returns a default, notice
- *  the static course_item_t for the default 
+ * Helper function that returns a course item given a course_id.
+ * Returns a default if no match is found.
  */
-course_item_t * lookup_course_by_id(char *course_id) {
+course_item_t *lookup_course_by_id(char *course_id) {
     static course_item_t NOT_FOUND_COURSE = {"NONE", "Requested Course Not Found"};
-
     int count = sizeof(course_db) / sizeof(course_db[0]);
-    for (int i = 0; i < count; i++){
+    for (int i = 0; i < count; i++) {
         if (strcasecmp(course_db[i].id, course_id) == 0)
             return &course_db[i];
     }
@@ -45,27 +39,19 @@ course_item_t * lookup_course_by_id(char *course_id) {
 }
 
 /*
- *  This function accepts a socket and processes requests from clients
- *  the server runs until stopped manually with a CTRL+C
+ * Processes incoming requests, continuously handling connections.
  */
-static void process_requests(int listen_socket){
+static void process_requests(int listen_socket) {
     cs472_proto_header_t header;
-    char msg_in_buffer[MAX_MSG_SIZE];
     char msg_out_buffer[MAX_MSG_BUFFER];
     int data_socket;
     course_item_t *details;
     int ret;
 
-    //again, not the best approach, need ctrl-c to exit
-    while(1){
-        //Do some cleaning to prepare for the next request given
-        //we are looping, lets not get the last request baggage
-        //inside of this request
-        memset(&header,0,sizeof(cs472_proto_header_t));
-        memset(msg_out_buffer,0,sizeof(msg_out_buffer));
-        memset(msg_in_buffer,0,sizeof(msg_in_buffer));
+    while (1) {
+        memset(&header, 0, sizeof(cs472_proto_header_t));
+        memset(msg_out_buffer, 0, sizeof(msg_out_buffer));
 
-        //Establish a connection
         data_socket = accept(listen_socket, NULL, NULL);
         if (data_socket == -1) {
             perror("accept");
@@ -74,111 +60,103 @@ static void process_requests(int listen_socket){
 
         printf("\t RECEIVED REQ...\n");
 
-        /*
-         * TODO:  Handle the rest of the loop, basically you need to:
-         *
-         *      call recv() to get the request from the client
-         * 
-         *      Here is some helper code after you receive data from the client.  This
-         *      helps get setup to actually process the client request
-         * 
-         *      cs472_proto_header_t *pcktPointer =  (cs472_proto_header_t *)recv_buffer;
-         *      uint8_t *msgPointer = NULL;
-         *      uint8_t msgLen = 0;
-         *      process_recv_packet(pcktPointer, recv_buffer, &msgPointer, &msgLen);
-         * 
-         */
-
-        //TODO:  DELETE THESE VARIABLES BELOW...
-        //SEE THE COMMENT ABOVE, THESE VARIABLES ARE JUST PUT IN HERE FOR NOW TO MAKE SURE
-        //THE STUB COMPILES
-        cs472_proto_header_t *pcktPointer;
+        // Receive data from the client
+        ret = recv(data_socket, recv_buffer, sizeof(recv_buffer), 0);
+        if (ret == -1) {
+            perror("recv");
+            close(data_socket);
+            continue;
+        }
+        
+        // Process received packet
+        cs472_proto_header_t *pcktPointer = (cs472_proto_header_t *)recv_buffer;
+        printf("length: %d \n", pcktPointer->len);
         uint8_t *msgPointer = NULL;
         uint8_t msgLen = 0;
+        process_recv_packet(pcktPointer, recv_buffer, &msgPointer, &msgLen);
+        printf("length: %hhu\n", msgLen);
 
-        //Now lets setup to process the request and send a reply, create a copy of the header
-        //also switch header direction
-        memcpy(&header, pcktPointer, sizeof(cs472_proto_header_t)); //start building rsp header
+        memcpy(&header, pcktPointer, sizeof(cs472_proto_header_t));
         header.dir = DIR_RECV;
-        switch(header.cmd){
+
+        switch (header.cmd) {
             case CMD_CLASS_INFO:
-                // sprintf(msg_out_buffer, class_msg, header.course);
                 details = lookup_course_by_id(header.course);
-                prepare_req_packet(&header,(uint8_t *)details->description, 
-                    strlen(details->description), send_buffer, sizeof(send_buffer));
+
+                uint8_t *payload = (uint8_t *)details->description;
+                header.len = strlen(details->description);  // Set to length of course info
+                
+                int packet_size = sizeof(cs472_proto_header_t) + header.len;
+                prepare_req_packet(&header, payload, header.len, send_buffer, sizeof(send_buffer));
+                if (send(data_socket, send_buffer, packet_size, 0) == -1) {
+                    perror("send");
+                    close(data_socket);
+                }
                 break;
-            case CMD_PING_PONG:
-                strcpy(msg_out_buffer,"PONG: ");
-                memcpy(msg_out_buffer + strlen(msg_out_buffer), msgPointer, msgLen);
-                prepare_req_packet(&header,(uint8_t *)msg_out_buffer, 
-                    strlen(msg_out_buffer), send_buffer, sizeof(send_buffer));
-                break;
+
+        case CMD_PING_PONG:
+            strcpy(msg_out_buffer, "PONG: ");
+            strncat(msg_out_buffer, (char *)msgPointer, msgLen);
+            printf("buffer: %lu\n", strlen(msg_out_buffer));
+            header.len = strlen(msg_out_buffer) + sizeof(header);
+            //header.len = strlen(msg_out_buffer) + sizeof(header)+1;
+            packet_size = header.len;
+            printf("Server header len: %hu, packet size: %d\n", header.len, packet_size);
+
+            prepare_req_packet(&header, (uint8_t *)msg_out_buffer, strlen(msg_out_buffer), send_buffer, sizeof(send_buffer));
+            if (send(data_socket, send_buffer, packet_size, 0) == -1) {
+                perror("send");
+                close(data_socket);
+            }
+            break;
+
             default:
-                perror("invalid command");
+                perror("Invalid command");
                 close(data_socket);
                 continue;
         }
 
-        /*
-         * TODO:  Now that we have processed things, send the response back to the 
-         *        client - hint - its in the send_buffer. also dont forget to close
-         *        the data_socket for the next request.
-         */       
+        close(data_socket);
     }
 }
 
 /*
- *  This function starts the server, basically creating the socket
- *  it will listen on INADDR_ANY which is basically all local
- *  interfaces, eg., 0.0.0.0
+ * Initializes and starts the server, binding to all interfaces.
  */
-static void start_server(){
+static void start_server() {
     int listen_socket;
     int ret;
-    
     struct sockaddr_in addr;
 
     unlink(SOCKET_NAME);
 
-    /* Create socket. */
     listen_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_socket == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    /* Bind socket to socket name. */
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(PORT_NUM);
 
-    ret = bind(listen_socket, (const struct sockaddr *) &addr,
-               sizeof(struct sockaddr_in));
+    ret = bind(listen_socket, (const struct sockaddr *) &addr, sizeof(struct sockaddr_in));
     if (ret == -1) {
-        perror("accept");
+        perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    /*
-     * Prepare for accepting connections. The backlog size is set
-     * to 20. So while one request is being processed other requests
-     * can be waiting.
-     */
     ret = listen(listen_socket, 20);
     if (ret == -1) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    //Now process requests, this will never return so its bad coding
-    //but ok for purposes of demo
     process_requests(listen_socket);
-
     close(listen_socket);
 }
 
-int main(int argc, char *argv[])
-{
-    printf("STARTING SERVER - CTRL+C to EXIT \n");
+int main(int argc, char *argv[]) {
+    printf("STARTING SERVER - CTRL+C to EXIT\n");
     start_server();
 }
